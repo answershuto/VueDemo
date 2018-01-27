f2b14db54410feedf2231b0e4f4392a9ebf370b4
answershuto <905307789@qq.com>
2018-01-27 21:22:42 +0800

    update 5.js

--- 5.js
+++ 5.js
@@ -18,21 +18,21 @@ class Watcher {
 let callbacks = [];
 let pending = false;
 
-function flushCallbacks () {
-    pending = false;
-    const copies = callbacks.slice(0);
-    callbacks.length = 0;
-    for (let i = 0; i < copies.length; i++) {
-        copies[i]();
-    }
-}
-
 function nextTick (cb) {
     callbacks.push(cb);
 
     if (!pending) {
         pending = true;
         setTimeout(flushCallbacks, 0);
+    }
+}
+
+function flushCallbacks () {
+    pending = false;
+    const copies = callbacks.slice(0);
+    callbacks.length = 0;
+    for (let i = 0; i < copies.length; i++) {
+        copies[i]();
     }
 }
 
